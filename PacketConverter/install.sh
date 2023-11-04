6d9baa2ae20a8bee3d5c953f98a3ccced39fa673
Alexander Valach <25947314+alexandervalach@users.noreply.github.com>
2023-11-04 11:44:21 +0100

    Update install.sh to be more generic

--- PacketConverter/install.sh
+++ PacketConverter/install.sh
@@ -1,54 +1,84 @@
 #!/bin/bash
+set -e
+
+# Define the service location
+service_location="/lib/systemd/system/"
+
+check_root() {
+    if [ "$UID" != 0 ]; then
+        echo "ERROR: Not a root user."
+        exit 1
+    fi
+}
+
+install_default() {
+    echo "INFO: Locally installing to this folder:"
+    mkdir -p debug
+    cd ./debug
+    cmake ..
+    make
+    make install
+    cd ..
+    echo "SUCCESS: Default installation completed."
+}
+
+install_daemon() {
+    if [ "$#" -lt 2 ]; then
+        service_name="packet_converter"
+    else
+        service_name="$2"
+    }
+
+    echo "INFO: Installing daemon service: $service_name"
+    touch "$service_location/$service_name.service"
+    echo "[Unit]" > "$service_location/$service_name.service"
+    echo "Description=$service_name daemon" >> "$service_location/$service_name.service"
+    echo "[Service]" >> "$service_location/$service_name.service"
+    echo "WorkingDirectory=$(pwd)" >> "$service_location/$service_name.service"
+    echo "ExecStart=$(pwd)/start.sh" >> "$service_location/$service_name.service"
+    echo "SyslogIdentifier=$service_name" >> "$service_location/$service_name.service"
+    echo "Restart=always" >> "$service_location/$service_name.service"
+    echo "RestartSec=5" >> "$service_location/$service_name.service"
+    echo "[Install]" >> "$service_location/$service_name.service"
+    echo "WantedBy=multi-user.target" >> "$service_location/$service_name.service"
+    systemctl daemon-reload
+    systemctl enable "$service_name.service"
+    echo "SUCCESS: Service $service_name successfully installed and is located at $service_location/$service_name.service"
+    echo "INFO: To disable it, run as super user:"
+    echo "  systemctl disable $service_name.service"
+}
+
+clear_installation() {
+    if [ "$#" -lt 2 ]; then
+        service_name="packet_converter"
+    else
+        service_name="$2"
+    }
+
+    echo "INFO: Clearing all install files for $service_name:"
+    rm -f ./packet_converter
+    rm -rf ./debug/
+    systemctl disable "$service_name.service"
+    rm "$service_location/$service_name.service"
+    echo "SUCCESS: All $service_name files are cleaned, and systemd daemon is disabled."
+}
+
 # Check root user
-if [ $UID != 0 ]; then
-    echo "ERROR: Not root user?"
-    exit 1
-fi
+check_root
+
 # Set what to do
-command="default"
+action="default"
 if [ "$#" -ge 1 ]; then
-	if [ "$1" == "daemon" ] || [ "$1" == "clear" ]; then
-    command=$1
-	fi
+    if [ "$1" == "daemon" ] || [ "$1" == "clear" ]; then
+        action="$1"
+    fi
 fi
 
-# do stuff
-if [ "$command" == "default" ]; then
-	echo "INFO: Locally installing to this folder:"
-	mkdir -p debug
-	cd ./debug
-	#cmake -DCMAKE_BUILD_TYPE=Debug .. build with debug symbols
-	cmake ..
-	make
-	make install
-	cd ..
-fi
-if [ "$command" == "daemon" ]; then
-  echo "WARNING: Installation should be done from PacketConverter folder by running ./install.sh daemon"
-	echo "INFO: Installing daemon service:"
-	touch /lib/systemd/system/packet_converter.service
-	echo "[Unit]" > /lib/systemd/system/packet_converter.service
-	echo "Description=PacketConverter daemon" >> /lib/systemd/system/packet_converter.service
-	echo "[Service]" >> /lib/systemd/system/packet_converter.service
-	echo "WorkingDirectory=`pwd`" >> /lib/systemd/system/packet_converter.service 
-	echo "ExecStart=`pwd`/start.sh" >> /lib/systemd/system/packet_converter.service
-	echo "SyslogIdentifier=packet_converter" >> /lib/systemd/system/packet_converter.service
-	echo "Restart=always" >> /lib/systemd/system/packet_converter.service
-	echo "RestartSec=5" >> /lib/systemd/system/packet_converter.service
-	echo "[Install]" >> /lib/systemd/system/packet_converter.service
-	echo "WantedBy=multi-user.target" >> /lib/systemd/system/packet_converter.service
-	systemctl daemon-reload
-	systemctl enable packet_converter.service
-	echo "SUCCESS: Service successfully installed and is located at /lib/systemd/system/packet_converter.service"
-	echo "INFO: To disable it, run as super user:"
-	echo "  systemctl disable packet_converter.service"
-	echo "INFO: In order to uninstall PacketConverter agent, run as super user:"
-	echo "  ./install.sh clear"
-fi
-if [ "$command" == "clear" ]; then
-	echo "INFO: Clearing all install files:"
-	rm ./packet_converter
-	rm -rf ./debug/
-	systemctl disable packet_converter.service
-	echo "SUCCESS: All PacketConverter files are cleaned and systemd daemon is disabled"
+# Perform the selected action
+if [ "$action" == "default" ]; then
+    install_default
+elif [ "$action" == "daemon" ]; then
+    install_daemon "$@"
+elif [ "$action" == "clear" ]; then
+    clear_installation "$@"
 fi
